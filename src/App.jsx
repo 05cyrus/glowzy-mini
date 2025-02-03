5683b1d6285fabc0f80b80883a78b0d87eb55154
Sumit Gusain <sumitgusain5311@gmail.com>
2025-02-03 11:45:24 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
+import Loader from './components/Loader/Loader';
 
 function App() {
+  const [showLoader, setShowLoader] = useState(false);
+
   return (
-    <div className="App">
-      <Header />
+    <div className={`app-container ${showLoader ? 'loader-active' : ''}`}>
+      {!showLoader ? <Header setShowLoader={setShowLoader} /> : <Loader setShowLoader={setShowLoader} />}
     </div>
   );
 }
 
 export default App;
-
